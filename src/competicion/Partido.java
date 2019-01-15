5e7cce3c08a5932ae7d1766d59def816f0f85458
Seba Garcia <Seba Garcia@SEBA-PC>
2019-01-15 13:08:11 +0100

    Implementada clase partido

--- src/competicion/Partido.java
+++ src/competicion/Partido.java
@@ -1,9 +1,31 @@
 package competicion;
 
-/**
- *
- * @author Seba Garcia
- */
-public class Partido {
+public class Partido
+{
+    private String local;
+    private String visitante;
+    private int[] goles;
     
+    public Partido(String l, String v)
+    {
+        local = l;
+        visitante = v;
+        goles = new int[2];
+    }
+    
+    public void setLocal(String j)
+    {
+        local = j;
+    }
+    
+    public void setVisitante(String j)
+    {
+        visitante = j;
+    }
+    
+    public void setGoles(int[] g)
+    {
+        goles[0] = g[0];
+        goles[1] = g[1];
+    }    
 }
